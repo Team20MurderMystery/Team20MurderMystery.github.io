279ed1fc1e57ecef91cef1d3d56071c2c43a2085
a033a532 <60753906+a033a532@users.noreply.github.com>
2020-11-02 17:05:47 -0600

    Object Interaction Templete
    This should be the templete for the object interaction. The background image is not scaled right but the if you hover over the book area it will show the book and give you an option to click on two buttons which will prompt alerts. (the image of the book is below the background image)

--- sources/index.js
+++ sources/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.body.style.backgroundColor = "Blue";
 });
+
+function brush(object) {
+    window.alert("You have collected fingerprints from the " + object)
+
+}
+function description(object) {
+    window.alert("Shows info for " + object)
+
+}

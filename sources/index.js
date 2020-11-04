84f4cf97ebe80212378dd3570c7b8d200e852fec
Marco Borth <borthmarco@ku.edu>
2020-11-04 16:29:53 -0600

    traversal between mansion and labroom complete. Labroom contains images for the main room and for fingerprint scanner and TCL. Buttons created for all sims and FPS and TLC open up sim displays with buttons to close out of the display.

--- sources/index.js
+++ sources/index.js
@@ -6,3 +6,44 @@ function description(object) {
     window.alert("Shows info for " + object)
 
 }
+
+function labroom() {
+  var x = document.getElementById("img3");
+  var y = document.getElementById("lab");
+  var z = document.getElementById("simulator");
+  if (y.style.display === "none") {
+    x.style.display = "none";
+    y.style.display = "block";
+  } else if (z.style.display === "block"){
+    z.style.display = "none";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+}
+
+function fps() {
+  document.getElementById('display').setAttribute("src", "imgs/lab_imgs/fps.jpeg")
+  var myDiv = document.getElementById("simulator");
+  myDiv.style.display = "block";
+  //var button = document.createElement('BUTTON');
+  var text = document.createTextNode("Scan Prints");
+  var image = ""
+  //button.appendChild(text);
+  //myDiv.appendChild(button);
+
+  var object = "teacup"
+}
+
+function tlc() {
+  document.getElementById('display').setAttribute("src", "imgs/lab_imgs/tlc.jpg")
+  var myDiv = document.getElementById("simulator");
+  myDiv.style.display = "block";
+  //var button = document.createElement('BUTTON');
+  var text = document.createTextNode("Isolate Mixture");
+  var image = ""
+  //button.appendChild(text);
+  //myDiv.appendChild(button);
+
+  var object = "teacup"
+}

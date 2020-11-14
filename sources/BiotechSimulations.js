20bef719885315b61208d671d66e1ca5eee1614b
Marco Borth <borthmarco@ku.edu>
2020-11-14 16:45:04 -0600

    ThinLayerChromatographer file deleted. TLC code has been migrated to Biotech Simulations.

--- sources/BiotechSimulations.js
+++ sources/BiotechSimulations.js
@@ -39,11 +39,11 @@ function endSimulation() {
 function runSimulation(nameOfSimulationToRun) {
     startSimulation();
 
+    // in this function, use the following line to get the screen for the simulation
+    var simulationScreen = document.getElementById("simulationScreen");
+
     if (nameOfSimulationToRun.id === "FPS") {
         // fps(); // call the js function that does fps
-
-        // in that function, use the following line to get the screen for the simulation
-        var simulationScreen = document.getElementById("simulationScreen");
 
         // THIS IS AN EXAMPLE --- DELETE THIS WHEN PLACING ACTUAL FPS JS FUNCTION CALL ABOVE
         var instructions = document.createElement('p1');
@@ -52,25 +52,61 @@ function runSimulation(nameOfSimulationToRun) {
     } else if (nameOfSimulationToRun.id === "TLC") {
         // tlc(); // call the js function that does tlc
 
-        // in that function, use the following line to get the screen for the simulation
-        var simulationScreen = document.getElementById("simulationScreen");
-
         // THIS IS AN EXAMPLE --- DELETE THIS WHEN PLACING ACTUAL TLC JS FUNCTION CALL ABOVE
         var instructions = document.createElement('p1');
         instructions.innerText = tutorial[1];
         simulationScreen.appendChild(instructions);
+
+        // Create and Apphend DNA Simulation Table with runDNA button within simulationDisplay
+        var table, row, cell, cellImage, text, newStartButton, simulationDisplay;
+        var tlcdirect = "imgs/Lab/";
+        var rowlen = 6; // set number of rows in table
+        var collen = 5; // set number of cells in each row
+
+        simulationDisplay = document.createElement("div");
+        simulationDisplay.id = "simulationDisplay";
+        simulationScreen.appendChild(simulationDisplay);
+
+        newStartButton = document.createElement("button");
+        newStartButton.id = "TLC"
+        newStartButton.innerText = "Run Simulation";
+        newStartButton.classList.add("runButton");
+        newStartButton.setAttribute("onClick", "runTLC();");
+
+        simulationDisplay.appendChild(newStartButton);
+
+        table = document.createElement("table");
+        table.id = "table";
+
+        // Create Initial Thin Layer Chromatography Display
+        for (var i = 0; i < rowlen; i++) {
+          row = document.createElement("tr");
+          for (var j = 0; j < collen; j++) {
+            cell = document.createElement("th");
+            if ( i === rowlen - 1) {
+              cell.append(j + 1); // Legend for TLC Test and Control Samples.
+            } else {
+              cellImage = document.createElement("img");
+              cellImage.id = i + "" + j;
+              cellImage.src = tlcdirect + "tlcempty.png";
+              cellImage.classList.add("objectImage");
+              cell.appendChild(cellImage);
+            }
+            row.appendChild(cell);
+          }
+          table.appendChild(row);
+        }
+
+        simulationDisplay.appendChild(table);
     } else if (nameOfSimulationToRun.id === "DNA") {
         // dna(); // call the js function that does dna
-
-        // in that function, use the following line to get the screen for the simulation
-        var simulationScreen = document.getElementById("simulationScreen");
 
         // THIS IS AN EXAMPLE --- DELETE THIS WHEN PLACING ACTUAL DNA JS FUNCTION CALL ABOVE
         var instructions = document.createElement('p1');
         instructions.innerText = tutorial[2];
         simulationScreen.appendChild(instructions);
 
-        //Create and Apphend DNA Simulation Table with runDNA button within simulationDisplay
+        // Create and Append DNA Simulation Table with runDNA button within simulationDisplay
         var table, row, cell, cellImage, text, newStartButton, simulationDisplay;
         var dnadirect = "imgs/Lab/";
         var rowlen = 14; // set number of rows in table
@@ -94,18 +130,18 @@ function runSimulation(nameOfSimulationToRun) {
         table = document.createElement("table");
         table.id = "table";
 
-        //Create Initial DNA Fingerprint Display.
+        // Create Initial DNA Fingerprint Display.
         for (var i = 0; i < rowlen; i++) {
           row = document.createElement("tr");
           for (var j = 0; j < collen; j++) {
             cell = document.createElement("th");
             if (i == 0) {
               if (j == 0) {
-                cell.append("Sample X");
+                cell.append("Sample X"); // Evidence DNA Sample.
               } else if (j < collen - 1) {
-                cell.append("Suspect " + j)
+                cell.append("Suspect " + j) // Suspect DNA Sample.
               } else {
-                cell.append("100 bp ladder")
+                cell.append("100 bp ladder") // morganvalues axis.
               }
             } else {
               if (j == collen - 1) {
@@ -120,7 +156,7 @@ function runSimulation(nameOfSimulationToRun) {
                 if (i == 1) {
                   cellImage.src = dnadirect + "dnasample.png";
                 } else {
-                  cellImage.src = dnadirect + "dnawhitespace.png";
+                  cellImage.src = dnadirect + "dnawhitespace.png"; // empty backgroundImage for Table.
                 }
                 cellImage.classList.add("objectImage");
                 cell.appendChild(cellImage);
@@ -137,8 +173,6 @@ function runSimulation(nameOfSimulationToRun) {
     } else {
         // blast(); // call the js function that does blast
 
-        // in that function, use the following line to get the screen for the simulation
-        var simulationScreen = document.getElementById("simulationScreen");
 
         // THIS IS AN EXAMPLE --- DELETE THIS WHEN PLACING ACTUAL BLAST JS FUNCTION CALL ABOVE
         var instructions = document.createElement('button');
@@ -149,24 +183,46 @@ function runSimulation(nameOfSimulationToRun) {
     }
 }
 
-//Create and Apphend DNA Simulation Table with runDNA button within simulationDisplay
-var table, row, cell, cellImage, text, newStartButton, simdisplay;
-var dnadirect = "imgs/Lab/";
-var rowlen = 14; // set number of rows in table
-var collen = 4; // set number of cells in each row
-// morganvalues indicate the position at which the dna fragments can be placed within the DNA simulation table.
-var morganvalues = [3000, 1500, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100];
-var morganindex = 0;
-// Fragments indicate whether a fragment is detected at the given morganvalues position
-// int > 0 -> fragment detected.
-var fragments = [ [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-                  [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-                ];
+function runTLC() {
+  var simdisplay = document.getElementById("simulationDisplay");
+  var table, row, cell, cellImage, text, newStartButton;
+  var tlcdirect = "imgs/Lab/";
+  var rowlen = 6; // set number of rows in table
+  var collen = 5; // set number of cells in each row
+
+  simdisplay.removeChild(document.getElementById("table"));
+
+  table = document.createElement("table");
+  table.id = "table";
+
+  for (var i = 0; i < 6; i++) {
+    row = document.createElement("tr");
+    for (var j = 0; j < 5; j++) {
+      cell = document.createElement("th");
+      if ( i == 5) {
+        cell.append(j + 1);
+      } else {
+        cellImage = document.createElement("img");
+        cellImage.id = i + "" + j;
+        if ((j == 0 || i - 1 == j-1 ) && (i > 0)) {
+          cellImage.src = tlcdirect + "tlcelement" + i + ".png";
+        } else {
+          cellImage.src = tlcdirect + "tlcempty.png";
+        }
+        cellImage.classList.add("objectImage");
+        cell.appendChild(cellImage);
+      }
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+
+  simdisplay.appendChild(table);
+}
 
 function runDNA() {
   var simdisplay = document.getElementById("simulationDisplay");
-  //Create and Apphend DNA Simulation Table with runDNA button within simulationDisplay
+  // Create and Append DNA Simulation Table with runDNA button within simulationDisplay
   var table, row, cell, cellImage, text, newStartButton;
   var dnadirect = "imgs/Lab/";
   var rowlen = 14; // set number of rows in table
@@ -189,6 +245,7 @@ function runDNA() {
       cellImage = document.getElementById(i + "" + j);
 
       if (fragments[j][i] > 0) {
+        // Update dnawhitespace image to dnafragment image when detected.
         cellImage.src = dnadirect + "dnafragment.png";
       }
     }
@@ -206,38 +263,6 @@ function brush(object) {
 function description(object) {
     window.alert("Shows info for " + object)
 
-}
-
-function disableObject(object){
-  document.getElementById(object).style.display = "none";
-}
-
-function enableObject(object){
-  document.getElementById(object).style.display = "block";
-}
-
-function labroom() {
-  var x = document.getElementById("mansion");
-  var y = document.getElementById("lab");
-  var z = document.getElementById("simulator");
-  if (y.style.display === "none") {
-    x.style.display = "none";
-    y.style.display = "block";
-    disableObject("img_book_div");
-    disableObject("img_plate_div");
-    disableObject("img_pipe_div");
-    disableObject("img_teacup_div");
-
-  } else if (z.style.display === "block"){
-    z.style.display = "none";
-  } else {
-    x.style.display = "block";
-    y.style.display = "none";
-    enableObject("img_book_div");
-    enableObject("img_plate_div");
-    enableObject("img_pipe_div");
-    enableObject("img_teacup_div");
-  }
 }
 
 function fps() {
@@ -294,12 +319,3 @@ function analyze() {
   }
   */
 }
-
-/*
-function findObject(object) {
-  for (i in inventory.length()) {
-    if object === inventory[i]
-      return object;
-  }
-}
-*/

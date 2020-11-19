6e374615ba7ae8262effcfd25a1d09fff1054336
Elvis Zhang <workpeng@vip.qq.com>
2020-11-19 13:34:59 -0600

    Add comments to BLAST related file

--- sources/TutorialControl.js
+++ sources/TutorialControl.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     current = 0
+    //Load tutorial pictures
     image = new Array, image[0] = "../imgs/TutorialPics/Tutorial1.jpg", image[1] = "../imgs/TutorialPics/Tutorial2.jpg", image[2] = "../imgs/TutorialPics/Tutorial3.jpg"
     document.querySelector("#image").src = image[current]
-    document.querySelector("#pre").addEventListener("click", () => {
+    document.querySelector("#pre").addEventListener("click", () => {//Previous picture switch
         if (current == 0) {
             current = 2
         }
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
             current -= 1
         document.querySelector("#image").src = image[current]
     })
-    document.querySelector("#next").addEventListener("click", () => {
+    document.querySelector("#next").addEventListener("click", () => {//Next picture switch
         if (current == 2) {
             current = 0
         }

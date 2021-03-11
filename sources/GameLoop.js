7ea3fa82926805d7e8c00170d285ea31ca923308
Christian Harris <c225h802@ku.edu>
2021-03-10 18:34:17 -0600

    Fixed broken object locations

--- sources/GameLoop.js
+++ sources/GameLoop.js
@@ -1,8 +1,3 @@
-var tutorial = ["To use the Fingerprint Scanner, insert evidence with detected fingerprints and press Scan to identify prints.",
-    "To use the Thin Layer Chromatographer, insert mixture evidence to seperate and identify elements.",
-    "To use the DNA Fingerprint Scanner, insert DNA Sample to seperate sample into DNA Fragments, and match the fingerprint given with the list of suspects."
-]
-
 var roomList;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -55,10 +50,10 @@ var roomsObj = [
 var labObjNames = ["FPS", "TLC", "DNA", "BLAST"];
 var labObjImgs = ["imgs/Lab/fps.jpg", "imgs/Lab/tlc.jpg", "imgs/Lab/dna.jpg", "imgs/Lab/blast.jpg"];
 var labObjLocs = [
-    [47, 4],
-    [69, 58],
-    [49, 55],
-    [41, 77]
+    [47, 14],
+    [69, 63],
+    [49, 60],
+    [41, 82]
 ];
 var labObjClues = [
     [
@@ -78,10 +73,10 @@ var labObjClues = [
 var studyObjNames = ["Book", "Plate", "Pipe", "Teacup"];
 var studyObjImgs = ["imgs/1_ProfessorLogan/items/Book.png", "imgs/1_ProfessorLogan/items/Plate.png", "imgs/1_ProfessorLogan/items/Pipe.png", "imgs/1_ProfessorLogan/items/Teacup.png"];
 var studyObjLocs = [
-    [80, 18],
-    [58, 70],
-    [80, 10],
-    [80, 55]
+    [80, 27],
+    [58, 75],
+    [80, 15],
+    [80, 60]
 ];
 var studyObjClues = [
             [
@@ -105,10 +100,10 @@ var studyObjClues = [
 var fireplaceObjNames = ["Vial", "Photo", "Mask", "Photo1"];
 var fireplaceObjImgs = ["imgs/1_ProfessorLogan/items/Vial.png", "imgs/1_ProfessorLogan/items/Warbuddies.png", "imgs/1_ProfessorLogan/items/Mask.png", "imgs/1_ProfessorLogan/items/Photograph.png"];
 var fireplaceObjLocs = [
-    [50, 5],
-    [50, 48],
-    [50, 85],
-    [50, 20]
+    [50, 15],
+    [50, 53],
+    [50, 87],
+    [50, 32]
 ];
 var fireplaceObjClues = [
             [
@@ -128,7 +123,7 @@ var fireplaceObjClues = [
 var kitchenObjNames = ["Tea"];
 var kitchenObjImgs = ["imgs/1_ProfessorLogan/items/KenyaAjiri.png"];
 var kitchenObjLocs = [
-    [30, 80]
+    [30, 85]
 ];
 var kitchenObjClues = [
             [
